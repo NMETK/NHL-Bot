f1024675141b2878e92ffd22ee73a1d1bbabaa08
Nathaniel Turner <nate.me.turner@gmail.com>
2023-09-16 20:23:47 -0600

    Now shows multiple game scores
    Now shows all game scores from selected date, bug with tie games pre 05/06 season.

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = "https://www.hockey-reference.com/boxscores/index.fcgi?month=11&day=10&year=2005"
+month = input("Input a Month: ")
+day = input("Input a Day: ")
+year = input("Input a Year: ")
+print(" ")
+
+URL = f"https://www.hockey-reference.com/boxscores/index.fcgi?month={month}&day={day}&year={year}"
 result = requests.get(URL).text
 doc = BeautifulSoup(result, "html.parser")
 
 bod = doc.body
-x = bod.find(class_="game_summary nohover")
+games = bod.find_all(class_="game_summary nohover")
 
-l = x.find(class_="loser")
-w = x.find(class_="winner")
+total = 0
 
-final = f"The {w.find_next().string} beat The {l.find_next().string}"
+for game in games:
+    total += 1
 
-print(final)
+print(f"{total} NHL games were played on {month}/{day}/{year}")
+print(" ")
+
+for game in games:
+    x = game.find(class_="loser")
+    l = x.a.string
+    y = game.find(class_="loser")
+    lf = y.find(class_="right").string
+
+    z = game.find(class_="winner")
+    w = z.a.string
+    n = game.find(class_="winner")
+    wf = n.find(class_="right").string
+
+    print(f"{l} {lf}")
+    print(f"{w} {wf}")
+    print(" ")
